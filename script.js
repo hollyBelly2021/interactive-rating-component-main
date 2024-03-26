a877959c92be0778dc92ff4f2ddf84caa883c1f8
Maea Matugas <matugasmaea@gmail.com>
2024-03-25 19:07:36 -0500

    js added

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 const ratingContainer = document.querySelector(".rating-container");
 
-// const descriptionContainer = document.querySelector(".description-container");
+// select the rating buttons
+const ratingButtons = document.querySelectorAll(".icon-circle");
 
-// descriptionContainer.style.display = "none";
+const submitButton = document.querySelector(".button");
+//calculate the length of each button
+const ratingButtonsLength = ratingButtons.length;
+//add eventlistener in to each buttons using for loop
+
+for (let i = 0; i < ratingButtonsLength; i++) {
+  ratingButtons[i].addEventListener("click", function () {
+    submitRating(this.innerHTML);
+  });
+}
+//will receive rating and submit it triggers the other html
+function submitRating(rating) {
+  //add an eventlistener to submit button and then modify the innerHTML of rating container.
+  submitButton.addEventListener("click", function () {
+    ratingContainer.innerHTML = `<section class="greeting-container">
+    <img
+      class="greeting-image"
+      src="./images/illustration-thank-you.svg"
+      alt=""
+    />
+    <div class="long-bubble">You selected ${rating} out of 5</div>
+    <div class="greeting-desc">
+      <h1>Thank you!</h1>
+      <p class="text">
+        We appreciate you taking the time to give a rating. If you ever
+        need more support, don’t hesitate to get in touch!
+      </p>
+    </div>
+  </section>`;
+  });
+}
